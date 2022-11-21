3138eed773717504b3f2de5fd5523904fdf74257
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2022-11-20 19:36:03 -0500

    Se agrega etiqueta repository

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -2,9 +2,12 @@ package com.platzi.platzimarket.persistence;
 
 import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -12,4 +15,25 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(Long idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(Long cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(Long idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Long idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
 }
--- src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,21 @@
 package com.platzi.platzimarket.persistence.crud;
 
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+
+    //Con Query Nativo
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoria(Long idCategoria);
+
+    //Con query Methods
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Long idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(Long cantidadStock, Boolean estado);
+
 }
